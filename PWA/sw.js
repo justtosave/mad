4976c4de90db9e925c354973125778f6bed7f415
justtosave <justtosave365@gmail.com>
2025-04-21 23:14:33 +0530

    Add files via upload

---
+++ PWA/sw.js
@@ -0,0 +1,24 @@
+// Service Worker for Simple PWA
+
+const CACHE_NAME = 'simple-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install service worker and cache assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
